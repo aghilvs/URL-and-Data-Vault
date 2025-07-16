9e40d9f0102a4fb4decce44ebc32ef06ef18b331
aghilvs <aghilvs@gmail.com>
2025-07-16 08:33:19 +0000

    Added functions to the save input, save url, delete buttons

--- index.js
+++ index.js
@@ -4,3 +4,43 @@ const inputBtn = document.getElementById("input-btn");
 const urlBtn = document.getElementById("url-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const listEl = document.getElementById("list-el");
+
+// Save data using input button
+inputBtn.addEventListener('click', function() {
+    myData.push(inputEl.value);
+    //Store data in the Local Storage
+    localStorage.setItem('myData', JSON.stringify(myData));
+    render(myData);
+})
+// Save the URL Data
+urlBtn.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myData.push(tabs[0].url)
+    })
+    render(myData)
+    localStorage.setItem('myData',JSON.stringify(myData));
+})
+const dataFromLocalStorage = JSON.parse(localStorage.getItem("myData"));
+
+if (dataFromLocalStorage) {
+    myData = dataFromLocalStorage;
+    render(myData);
+}
+
+//Function render
+
+function render(data) {
+    let listItems = '';
+    for (let i=0;i<data.length;i++) {
+        listItems += `<li>
+                            <a target="_blank" href="${data[i]}">${data[i]}</a>
+                      </li>`;
+    }
+    listEl = listItems;
+}
+// Delete all the data with a double click
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear();
+    myData = [];
+    render(myData)
+})
