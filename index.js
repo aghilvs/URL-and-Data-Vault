4105e4d3c5a32761366e9eaf9ed3aeec16073c04
aghilvs <aghilvs@gmail.com>
2025-07-16 05:35:14 +0000

    Created the input, save input, save url, delete all buttons, styled them.

---
+++ index.js
@@ -0,0 +1,6 @@
+let myData = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const urlBtn = document.getElementById("url-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const listEl = document.getElementById("list-el");
